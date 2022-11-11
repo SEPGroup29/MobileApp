2b7160403ecc4d4728a72595a8a8e055ad3e0095
Yasiru Lakshan <yasiru1999@gmail.com>
2022-11-11 17:26:16 +0530

    Logo added

--- views/loginView/LoginView.js
+++ views/loginView/LoginView.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import {Text, View, StyleSheet} from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 function LoginView() {
-    return (
-        <View style={styles.container}>
-            <Text>LoginView</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image source={require("../../assets/logo192.png")} style={styles.logo} />
+      <Text>LoginView</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1F7A8C",
+  },
+  logo: {
+    width: "20%",
+    height: "10%",
+    margin: 10,
+  },
 });
 
 export default LoginView;
