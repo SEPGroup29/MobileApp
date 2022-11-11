d3d055abde958811615a0d435d516be269e962a6
Yasiru Lakshan <yasiru1999@gmail.com>
2022-11-11 17:50:29 +0530

    Input Fields Completed

--- views/loginView/LoginView.js
+++ views/loginView/LoginView.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, TextInput } from "react-native";
 
 function LoginView() {
+  const [email, setEmail] = React.useState("");
+
   return (
     <View style={styles.container}>
       <Image source={require("../../assets/logo192.png")} style={styles.logo} />
       <Text style={styles.titleText}>LOGIN</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter Email"
+        placeholderTextColor="#aaa"
+      />
+      <TextInput
+        style={[styles.textInput, styles.passwordInput]}
+        placeholder="Enter Password"
+        placeholderTextColor="#aaa"
+        secureTextEntry={true}
+      />
     </View>
   );
 }
@@ -26,6 +39,19 @@ const styles = StyleSheet.create({
     fontSize: 25,
     color: "white",
     fontWeight: "bold",
+  },
+  textInput: {
+    color: "white",
+    height: 40,
+    fontSize: 20,
+    textTransform: "lowercase",
+    top: 20,
+    width: "80%",
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+  },
+  passwordInput: {
+    top: 40,
   },
 });
 
