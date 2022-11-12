1e1c58ca77694e0c18892cd8a0350e85488b2f3b
Yasiru Lakshan <yasiru1999@gmail.com>
2022-11-12 11:31:06 +0530

    Fuel Type Added

--- views/fuelRefillPage/FuelFillView.js
+++ views/fuelRefillPage/FuelFillView.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
 
 function FuelFillView() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.heading}>672923076V</Text>
       <View style={styles.vehicleNumberContainer}>
         <Text style={[styles.heading, styles.vehicleNumber]}>AXQ-6484</Text>
       </View>
-      <Text style={styles.vehicleType}>Motor Bike</Text>
-    </View>
+      <Text style={styles.vehicleType}>Motor Bike - Petrol</Text>
+      <View style={styles.fuelTypeContainer}>
+        <View>
+          <Text style={styles.fuelTypeHeader}>Fuel Type: </Text>
+        </View>
+        <View>
+          <Text style={styles.fuelType}>Petrol</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,6 +26,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    alignContent: "center",
     backgroundColor: "#1F7A8C",
   },
   heading: {
@@ -42,6 +51,24 @@ const styles = StyleSheet.create({
     fontSize: 15,
     top: 15,
     color: "white",
+  },
+  fuelTypeContainer: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    flex: 1,
+    top: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fuelTypeHeader: {
+    fontSize: 20,
+    color: "white",
+  },
+  fuelType: {
+    fontSize: 40,
+    color: "orange",
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
 });
 
